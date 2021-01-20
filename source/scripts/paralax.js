8a80ee5148aaee3fa0a44f668b2aaa6825485452
Anton Tkachuk <chiga2030@gmail.com>
2021-01-20 11:16:18 +0300

    animation for lamp is done

--- source/scripts/paralax.js
+++ source/scripts/paralax.js
@@ -1,65 +1,25 @@
-// const rightLamp = document.querySelector('.lamps-right__front-lamp');
-// // let scroll = window.pageYOffset;
-// console.log(rightLamp);
-// console.log('ololo');
-// let position = -18;
-
-// const scrollHeight = () => {
-//   if(window.pageYOffset >= 600) {
-//     console.log(window.pageYOffset/16)
-//     rightLamp.style.transform = `translate(6.5em, calc(${position + (window.pageYOffset/16)}em - ${window.pageYOffset}px))`;
-//   }
-
-// };
-
-
-
-
-
-//   let curent = window.pageYOffset;
-
-// const scrollHeight = () => {
-//     if((curent + window.pageYOffset) > curent) {
-//       console.log('вниз')
-//     } else console.log('вверх')
-//   }
-
-// window.addEventListener('scroll', scrollHeight);
-
-// console.log(window.scrollY);
-
-// document.addEventListener('scroll', (evt)=>{
-//  console.log(evt.deltaY);
-// });
-
-
-// document.addEventListener('wheel', (evt)=>{
-//   if (window.pageYOffset >= 430 && window.pageYOffset <= 840) {
-//       console.log(evt.deltaY)
-//   }
-// });
-
-
-// window.pageYOffset
-
-
-
-// const direction = () => {
-//   // document.addEventListener('scroll', upOrDown);
-//   return upOrDown()
-// }
-
-let pos = 0;
+const rightLamp = document.querySelector('.lamps-right__front-lamp');
 
 const direction = () => {
-  if (window.pageYOffset > pos) {
-    console.log('go down')
-  } else console.log('go up')
-  pos = window.pageYOffset;
-}
-
-document.addEventListener('scroll', () => {
-  if (window.pageYOffset >= 430 && window.pageYOffset <= 840) {
-    return direction();
+  let lampPosition = -74;
+  if (document.documentElement.clientWidth < 1440) {
+    lampPosition = -65;
   }
-});
+
+  lampPosition = lampPosition + (window.pageYOffset / 16);
+  rightLamp.style.transform = `translateY(${lampPosition}em)`;
+};
+
+const checkWievPort = func => {
+  if ((document.documentElement.clientWidth >= 1440 &&
+      window.pageYOffset >= 900 &&
+      window.pageYOffset <= 1200) ||
+      (document.documentElement.clientWidth < 1440 &&
+      window.pageYOffset >= 700 &&
+      window.pageYOffset <= 900)) {
+    return func();
+  }
+};
+
+document.addEventListener('scroll', () => checkWievPort(direction));
+document.addEventListener('DOMContentLoaded', () => checkWievPort(direction));
